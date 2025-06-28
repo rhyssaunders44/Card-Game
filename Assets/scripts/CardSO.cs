3a4b47294eadcb66f01ae5716d93f77ecc7123fc
rhyssaunders44 <78834755+rhyssaunders44@users.noreply.github.com>
2025-06-28 18:14:30 +1000

    Dan I did a thing

--- Assets/scripts/CardSO.cs
+++ Assets/scripts/CardSO.cs
@@ -3,12 +3,12 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "CardSO", menuName = "Scriptable Objects/CardSO")]
 public class CardSO : ScriptableObject
 {
-    public CardManager.CardAbilities[] cardAbilities;
-    public CardManager.CardType[] cardTypes;
     public CardManager.Faction cardFaction;
-    
     public int baseStrength;
     public int baseDefense;
+    public CardManager.CardAbilities[] cardAbilities;
+    public CardManager.CardType[] cardTypes;
+
 
 
 }
