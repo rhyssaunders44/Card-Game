1b4aaec9adea65e913ec9a161de464e7edc5c44e
rhyssaunders44 <78834755+rhyssaunders44@users.noreply.github.com>
2025-06-28 18:01:35 +1000

    1 deck example, 1 card example

--- Assets/scripts/CardManager.cs
+++ Assets/scripts/CardManager.cs
@@ -16,7 +16,7 @@ public class CardManager : MonoBehaviour
 
     public enum Faction
     {
-        bingus, aboba
+        any, bingus, aboba
     }
 
     public List<DeckSO> decks;
--- Assets/scripts/DeckSO.cs
+++ Assets/scripts/DeckSO.cs
@@ -3,5 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "DeckSO", menuName = "Scriptable Objects/DeckSO")]
 public class DeckSO : ScriptableObject
 {
-    
+    public CardManager.Faction faction;
+    public int[] cardCounts;
+    public CardSO[] uniqueCards;
 }
